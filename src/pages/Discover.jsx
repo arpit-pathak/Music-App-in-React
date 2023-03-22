91c60153c4d330249d9dcac4e810275d475dfe67
code-today <blogarpit21@gmail.com>
2023-03-22 23:59:28 +0530

    updated error.js page

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -5,12 +5,12 @@ const Discover = () => {
   console.log(genres);
   return (
     <div className="flex flex-col">
-      <div className="w-full flex flex-col justify-between items-center sm:bg-blue-400 sm:flex-row mt-4 mb-10 bg-red-400">
+      <div className="w-full flex flex-col justify-between items-center sm:flex-row mt-4 mb-10">
         <h2 className="font-bold text-3xl text-white text-left">Discover</h2>
         <select
           onChange={() => {}}
           value=""
-          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+          className="bg-black text-gray-300 p-2 text-sm rounded-lg outline-none sm:mt-0 mt-5"
         >
           {genres.map((genres) => (
             <option key={genres.value} value={genres.value}>
@@ -18,6 +18,11 @@ const Discover = () => {
             </option>
           ))}
         </select>
+      </div>
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {[1, 2, 3, 4, 5, 6, 7, 8].map((song) => (
+          <SongCard />
+        ))}
       </div>
     </div>
   );
