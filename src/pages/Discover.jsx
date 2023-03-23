942c27519f08e7aca04bf061b2bc9aaa3a8f9388
code-today <blogarpit21@gmail.com>
2023-03-23 19:47:48 +0530

    configure shazam api

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -20,7 +20,7 @@ const Discover = () => {
         </select>
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {[1, 2, 3, 4, 5, 6, 7, 8].map((song) => (
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song) => (
           <SongCard />
         ))}
       </div>
